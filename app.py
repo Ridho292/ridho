7f968ae75576c88dd007c3091245d06fe7f4fbf7
Ridho292 <muhammad.ridho123@icloud.com>
2024-04-02 10:07:46 +0700

    ini commit ke 2

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ tinggi = float(input("input tinggi segitiga :"))
 
 print("luas segitiga = ", hitungLuasSegitiga(alas,tinggi))
 print()
+
+print("## program python menghitung luas persegi panjang ##")
+print("#############################################")
+print()
+
+def hitungPersegiPanjang(p,l)
+    return round (p*l,2)
+
+panjang = float(input("input panjang : "))
+lebar = float(input("input lebar : "))
+print("luas persegi panjang = ",hitungPersegiPanjang(panjang,lebar))
